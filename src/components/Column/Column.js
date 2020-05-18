bab1f6d3b8b20cafc9db3634bc27e01d8b8c3f83
Katarzyna <kkazimierczuk@gmail.com>
2020-05-18 19:19:42 +0200

    add new card - cardsRedux.js

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 import Card from '../Card/Card.js';
 import { settings } from '../../data/dataStore.js';
 import Icon from '../Icon/Icon.js';
+import Creator from '../Creator/Creator.js';
 
 class Column extends React.Component {
   /*
@@ -16,6 +17,7 @@ class Column extends React.Component {
     title: PropTypes.string,
     cards: PropTypes.array,
     icon: PropTypes.node,
+    addCard: PropTypes.func,
   }
 
   static defaultProps = {
@@ -39,7 +41,7 @@ class Column extends React.Component {
   */
 
   render() {
-    const {title, icon, cards} = this.props;
+    const {title, icon, cards, addCard} = this.props;
     return (
       <section className={styles.component}>
         <h3 className={styles.title}>{title}
@@ -51,11 +53,11 @@ class Column extends React.Component {
             <Card key={cardData.id} {...cardData} />
           ))}
         </div> 
-        {/*}
+        
         <div className={styles.creator}>
-          <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
+          <Creator text={settings.cardCreatorText} action={addCard}/>
         </div> 
-          */}
+        
       </section>  
     );
   }
--- src/components/List/ListContainer.js
+++ src/components/List/ListContainer.js
@@ -1,10 +1,22 @@
 import {connect} from 'react-redux';
 import List from './List';
+import {getColumnsForList, createActionAddColumn} from '../../redux/columnsRedux.js';
 
+/*
 export const getColumnsForList = ({columns}, listId) => columns.filter(column => column.listId == listId);
+*/
 
 const mapStateToProps = (state, props) => ({
   columns: getColumnsForList(state, props.id),
 });
 
-export default connect(mapStateToProps)(List);
+/* funkcja dodaje propsy komponentu – jednak ich wartościami nie są dane ze stanu, ale funkcje wysyłające akcje do magazynu. W tym wypadku props addColumn będzie zawierał funkcję, przyjmującą jeden argument – title. Na podstawie tego argumentu zostanie wykonana funkcja dispatch (która jest argumentem funkcji mapDispatchToProps), wysyłająca akcję do magazynu.
+*/
+const mapDispatchToProps = (dispatch, props) => ({
+  addColumn: title => dispatch(createActionAddColumn({
+    listId: props.id,
+    title,
+  })),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(List);
